c6e1cc5bc2a0406303556c82f18879f968241bd0
tao.kaili <tao.kaili@immomo.com>
2020-04-18 12:51:08 +0800

    master

--- Leetcode_ex/src/main/java/org/leetcode/test/day01/SparseArrayTest.java
+++ Leetcode_ex/src/main/java/org/leetcode/test/day01/SparseArrayTest.java
@@ -6,4 +6,7 @@ package org.leetcode.test.day01;
  * @date : 2020/4/18 下午12:36
  */
 public class SparseArrayTest {
+    public static void main(String[] args) {
+        System.out.println("main");
+    }
 }
